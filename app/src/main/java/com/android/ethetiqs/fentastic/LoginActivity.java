de5b04fc2827c949d95d830d00c45ee139705466
YuxuanXia1998 <38146603+YuxuanXia1998@users.noreply.github.com>
2019-11-20 00:16:46 -0500

    added user register credentials to db

--- app/src/main/java/com/android/ethetiqs/fentastic/LoginActivity.java
+++ app/src/main/java/com/android/ethetiqs/fentastic/LoginActivity.java
@@ -1,21 +1,29 @@
 package com.android.ethetiqs.fentastic;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
 
     EditText usernameText;
     EditText passwordText;
     Button loginButton;
     Button registerButton;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +32,14 @@ public class LoginActivity extends AppCompatActivity {
         // get UI components by their Id
         usernameText = (EditText) findViewById(R.id.input_text_username);
         passwordText = (EditText) findViewById(R.id.input_text_password);
-        loginButton = findViewById(R.id.button_login);
+        //loginButton = findViewById(R.id.button_login);
 
+        mAuth = FirebaseAuth.getInstance();
 
         // set page title
         setTitle("Login");
+        findViewById(R.id.button_login).setOnClickListener(this);
+        findViewById(R.id.button_reg).setOnClickListener(this);
 
         /*There are two ways to define onClick method:
          1. create a onClickListener and assign the listener to the UI component in code.
@@ -36,22 +47,65 @@ public class LoginActivity extends AppCompatActivity {
          when both methods are used, first method has higher priority */
 
         // first method
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class);
-                loginIntent.putExtra("Message", "Hello World");
-                startActivity(loginIntent);
-            }
-        });
+//        loginButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                userLogin();
+//                Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class);
+//                loginIntent.putExtra("Message", "Hello World");
+//                startActivity(loginIntent);
+//            }
+//        });
+//
+//        registerButton = findViewById(R.id.button_reg);
+//        registerButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intentRegister = new Intent(LoginActivity.this, RegisterActivity.class);
+//                startActivity(intentRegister);
+//
+//            }
+//        });
+    }
 
-        registerButton = findViewById(R.id.button_reg);
-        registerButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intentRegister = new Intent(LoginActivity.this, RegisterActivity.class);
-                startActivity(intentRegister);
+    protected void userLogin() {
+        String email = usernameText.getText().toString().trim();
+        String password = passwordText.getText().toString().trim();
 
+        if (email.isEmpty()) {
+            usernameText.setError("Email is required");
+            usernameText.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            usernameText.setError("Please enter a valid email");
+            usernameText.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty()) {
+            passwordText.setError("Password is required");
+            passwordText.requestFocus();
+            return;
+        }
+
+        if (password.length() < 6) {
+            passwordText.setError("Minimum length of a password should be 6");
+            passwordText.requestFocus();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(), "User registered successfully", Toast.LENGTH_SHORT).show();
+                    Intent loginIntent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(loginIntent);
+                } else {
+                    Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
@@ -59,5 +113,17 @@ public class LoginActivity extends AppCompatActivity {
     // second method, only for example
     public void startMain(View view) {
         Toast.makeText(LoginActivity.this, "Login Button Clicked", Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button_login:
+                userLogin();
+                break;
+
+            case R.id.button_reg:
+                startActivity(new Intent(this, RegisterActivity.class));
+                break;
+        }
     }
 }
--- app/src/main/java/com/android/ethetiqs/fentastic/RegisterActivity.java
+++ app/src/main/java/com/android/ethetiqs/fentastic/RegisterActivity.java
@@ -1,28 +1,103 @@
 package com.android.ethetiqs.fentastic;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class RegisterActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+
+import java.util.regex.Pattern;
+
+public class RegisterActivity extends AppCompatActivity implements View.OnClickListener{
     Button submitButton;
+    EditText editTextEmail, editTextPassword;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        editTextEmail = (EditText) findViewById(R.id.email_text_input_edittext);
+        editTextPassword = (EditText) findViewById(R.id.password_edit_text);
+        mAuth = FirebaseAuth.getInstance();
 
-        submitButton = findViewById(R.id.button_sub);
-        submitButton.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.button_sub).setOnClickListener(this);
+        findViewById(R.id.button_login_fromreg).setOnClickListener(this);
+
+//        submitButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                registerUser();
+//                Intent intentLogin = new Intent(RegisterActivity.this, LoginActivity.class);
+//                startActivity(intentLogin);
+//
+//            }
+//        });
+    }
+
+    private void registerUser() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if (email.isEmpty()) {
+            editTextEmail.setError("Email is required");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            editTextEmail.setError("Please enter a valid email");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty()) {
+            editTextPassword.setError("Password is required");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        if (password.length() < 6) {
+            editTextPassword.setError("Minimum length of a password should be 6");
+            editTextPassword.requestFocus();
+            return;
+        }
+
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
-            public void onClick(View view) {
-                Intent intentLogin = new Intent(RegisterActivity.this, LoginActivity.class);
-                startActivity(intentLogin);
-
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(), "User registered successfully", Toast.LENGTH_SHORT).show();
+                    Intent loginIntent = new Intent(RegisterActivity.this, LoginActivity.class);
+                    startActivity(loginIntent);
+                } else {
+                    Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button_sub:
+                registerUser();
+                break;
+            case R.id.button_login_fromreg:
+                startActivity(new Intent(this, LoginActivity.class));
+                break;
+        }
+    }
+
+
 }
